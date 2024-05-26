13d93ed40664165310ac358183991af7a2cf1dbf
Satyam Birla <159645371+satyambirla18@users.noreply.github.com>
2024-05-26 13:08:59 +0530

    Time: 5 ms (17.4%), Space: 9.8 MB (32.84%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums){
+        int n = nums.size();
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<i;j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                }
+            }
+        }
+    }
+};
