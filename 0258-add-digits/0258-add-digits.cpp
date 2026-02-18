e20f61bb4efbdba9b238103f264c2c01c07886d7
Satyam Birla <159645371+satyambirla18@users.noreply.github.com>
2026-02-18 08:45:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.32%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while (num > 9){
+            int ans = 0;
+            int rem;
+            while(num != 0){
+                rem = num % 10;
+                num /= 10;
+                ans += rem;
+            }
+            num = ans;
+        }
+        return num;
+    }
+};
