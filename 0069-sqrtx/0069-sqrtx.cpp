eb87253f89fd51433db19c215179fe198c029c92
Satyam Birla <159645371+satyambirla18@users.noreply.github.com>
2026-03-22 19:48:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.74%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x <= 1) return x;
-        long long i = 1;
-        while (i * i <= x) {
-            i++;
+        if(x < 2) return x;
+        int s = 0;
+        int e = x;
+        int ans = 0;
+        while(s <= e){
+            int mid = s+(e-s)/2;
+            if(mid == x/mid){
+                ans = mid;
+                break;
+            }
+            else if(mid < x/mid){
+                ans = mid;
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
         }
-        return i - 1;
+        return ans;
     }
 };
